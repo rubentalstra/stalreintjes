70745846238e53772656d14db78a4d25d593b2f8
Ruben Talstra <RubenTalstra1211@outlook.com>
2020-01-13 16:40:31 +0100

    jap

--- contact.php
+++ contact.php
@@ -32,31 +32,31 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 		$emailBody = "
 			<html>
 			<head>
-				<title>$email is contacting you</title>
+				<title>$email stuurt een bericht</title>
 			</head>
 			<body style=\"background-color:#fafafa;\">
 				<div style=\"padding:20px;\">
-					Date: <span style=\"color:#888\">$date</span>
+					Datum: <span style=\"color:#888\">$date</span>
 					<br>
-					Email: <span style=\"color:#888\">$firstname</span>
+					Voornaam: <span style=\"color:#888\">$firstname</span>
 					<br>
-					Email: <span style=\"color:#888\">$lastname</span>
+					Achternaam: <span style=\"color:#888\">$lastname</span>
 					<br>
-					Email: <span style=\"color:#888\">$email</span>
+					E-mailadres: <span style=\"color:#888\">$email</span>
 					<br>
-					Message: <div style=\"color:#888\">$message</div>
+					Bericht: <div style=\"color:#888\">$message</div>
 				</div>
 			</body>
 			</html>
 		";
 
-		$headers = 	'From: Contact Form <contact@mydomain.com>' . "\r\n" .
+		$headers = 	'From: Contact pagina <contact@stalreintjes.nl>' . "\r\n" .
     				"Reply-To: $email" . "\r\n" .
     				"MIME-Version: 1.0\r\n" . 
 					"Content-Type: text/html; charset=iso-8859-1\r\n";
 
 		$to = 'rubentalstra1211@outlook.com';
-		$subject = 'Contacting you';
+		$subject = 'Stuurt een bericht';
 
 		if (mail($to, $subject, $emailBody, $headers)) {
 			$sent = true;	
