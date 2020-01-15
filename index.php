7040e99b4ef45371ea8c5a07103f8200c87b9c27
Ruben Talstra <RubenTalstra1211@outlook.com>
2020-01-15 09:56:57 +0100

    uh yeah

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="css/jquery.fancybox.min.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="fonts/flaticon-1/font/flaticon.css">
     <link rel="stylesheet" href="css/aos.css">
 
     <!-- MAIN CSS -->
