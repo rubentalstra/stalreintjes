30ef8bf4f57cfe54ecc338a6ae9b190a1f7ca061
Ruben Talstra <RubenTalstra1211@outlook.com>
2020-01-15 10:35:33 +0100

    Update about.php
    dobble weg halen

--- about.php
+++ about.php
@@ -92,31 +92,6 @@
             </div>
         </div>
 
-        <div id="about" class="site-section">
-            <div class="container">
-                <div class="row align-items-stretch">
-                    <div class="col-lg-4">
-                        <div class="h-100 bg-white box-29291">
-                            <h2 class="heading-39291">Welkom bij <br> Stal Reintjes</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo mollitia id ea ab in! Nam eligendi distinctio, vitae.</p>
-                            <p>Alias odit ipsam quas unde obcaecati molestiae consequatur numquam cupiditate perferendis facere, nulla nemo id, accusantium corrupti tempora.</p>
-
-                            <p class="mt-5">
-                                <span class="d-block font-weight-bold text-black">Isabel en Milou Reintjes</span>
-                                <span class="d-block font-weight-bold text-muted">:)</span>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="owl-carousel owl-3">
-                            <img src="images/milou_en_isabel.jpg" alt="Image" class="img-fluid">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="site-section">
             <div class="container">
                 <div class="row">
