9b803bab8c0dc3bc75defc4be53d983c3f19cbef
Ruben Talstra <RubenTalstra1211@outlook.com>
2020-01-29 10:28:05 +0100

    apple touch icon

--- contact.php
+++ contact.php
@@ -83,6 +83,15 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="shortcut icon" href="images/stalreintjes.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="images/apple/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="images/apple/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="images/apple/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="images/apple/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="images/apple/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="images/apple/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="images/apple/apple-touch-icon-180x180.png" />
 
     <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
 
